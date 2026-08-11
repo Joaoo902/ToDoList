a1934dc0824c6a9d4d25deba260d77deba191225
Joaoo <joaofranciscoprado1@gmail.com>
2026-08-11 06:33:40 -0300

    feat: adiciona criação dinamica de tarefas com JS

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-ainda n fiz o javascript, mas vou fazer em breve
+// pegando referencia do HTML usando ID que criei
+const formTarefa = document.getElementById('form-tarefa');
+const inputTarefa = document.getElementById('task-input');
+const listaTarefas = document.getElementById('lista-de-tarefas');
+
+// detecta alguma tarefa que o ususário coloca
+formTarefa.addEventListener('submit', function(event) {
+    event.preventDefault(); // previne o carregamento automatico da pagina 
+
+    const textoTarefa = inputTarefa.value.trim(); // pega o valor do input e remove espaços em branco
+
+    if (textoTarefa == '') { // verifica se o input não está vazio, se tiver vazio não faz nada
+        return;
+    }
+
+    // cria um elemento li na memória do navegador
+    const novaLi = document.createElement('li');
+
+    // cria a estrutura interna com o texto da tarefa e o botão excluir
+    novaLi.innerHTML = `
+        <span>${textoTarefa}</span>
+        <button class="btn-excluir">X</button>
+    `;
+
+    // Coloca a <li> dentro da <ul> no HTML
+    listaTarefas.appendChild(novaLi);
+
+    // Limpa o campo e coloca o cursor novamente nele
+    inputTarefa.value = '';
+    inputTarefa.focus();
+});
