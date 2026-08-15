72ee421ae5b8082f7ddc108120b85a8c7a4e7db8
Joaoo <joaofranciscoprado1@gmail.com>
2026-08-15 19:02:41 -0300

    feat: salvando em banco de dados no navegador

--- script.js
+++ script.js
@@ -37,3 +37,68 @@ listaTarefas.addEventListener('click', function (event) {
         liParaRemover.remove();
     }
 });
+
+const formTarefa = document.getElementById('form-tarefa');
+const inputTarefa = document.getElementById('task-input');
+const listaTarefas = document.getElementById('lista-de-tarefas');
+
+function obterTarefasSalvas() {
+    const tarefas = localStorage.getItem('minhasTarefas');
+    return tarefas ? JSON.parse(tarefas) : [];
+}
+
+function salvarTarefas(tarefas) {
+    localstorage.setItem('minhasTarefas', JSON.stringify(tarefas));
+}
+
+function criarElementoTarefa(texto) {
+    const novaLi = document.createElement('li');
+    novaLi.innerHTML = `
+        <spam>${texto}</spam>
+        <button class="btn-excluir">X</button>
+    `;
+    listaTarefas.appendChild(novaLi);
+}
+
+function carregarTarefas() {
+    const tarefas = obterTarefasSalvas();
+    tarefas.forEach(function (tarefa) {
+        cirarElementoTarefa(tarefa);
+    });
+}
+
+formTarefa.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const textoTarefa = inputTarefa.value.trim();
+
+    if (textoTarefa === '') {
+        return;
+    }
+
+    criarElementoTarefa(textoTarefa);
+
+    const tarefas = obterTarefassalvas();
+    tarefas.push(textoTarefa);
+    salvarTarefas(tarefas);
+
+    inputTarefa.value = '';
+    inputTarefa.focus();
+});
+
+listaTarefas.addEventListener('click', function (event) {
+    if (event.target.calssList.contains('btn-excluir')) {
+        const liParaRemover = event.target.parentElement;
+        const textoParaRemover = liParaRemover.querySelector('span').textContent;
+
+        liParaRemover.remove();
+
+        let tarefas = obterTarefasSalvas();
+        tarefas = tarefas.filter(function (tarefa) {
+            return tarefa !== textoParaRemover;
+        });
+        salvarTarefas(tarefas);
+    }
+});
+
+carregarTarefas();
